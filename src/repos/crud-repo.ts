ddb5c08b51b97e77f3420a4856307107b3cf4b2e
Drew Peters <peters.drew.dp@gmail.com>
2020-04-24 15:47:37 -0400

    Added crud interface for other repos to inherit

---
+++ src/repos/crud-repo.ts
@@ -0,0 +1,7 @@
+export interface CrudRepository<Type> {
+    getAll(): Promise<Type[]>;
+    getById(id: number): Promise<Type>;
+    save(newObj: Type): Promise<Type>;
+    update(updatedObj: Type): Promise<boolean>;
+    deleteById(id: number): Promise<boolean>;
+}
