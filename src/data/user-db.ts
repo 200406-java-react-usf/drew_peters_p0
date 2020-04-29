eff7b2ef81a2a0ad34404ab1f3d6281392b12074
Drew Peters <peters.drew.dp@gmail.com>
2020-04-29 13:19:24 -0400

    updated user db to comply with crud req

--- src/data/user-db.ts
+++ src/data/user-db.ts
@@ -2,9 +2,9 @@ import { User } from '../models/user';
 let id = 1;
 
 export default [
-    new User(id++, 'aanderson', 'password', 'Alice', 'Anderson', 'client'),
-    new User(id++, 'bbailey', 'password', 'Bob', 'Bailey', 'client'),
-    new User(id++, 'ccountryman', 'password', 'Charlie', 'Countryman', 'client'),
-    new User(id++, 'ddavis', 'password', 'Daniel', 'Davis', 'admin'),
-    new User(id++, 'eeinstein', 'password', 'Emily', 'Einstein', 'client')
+    new User(id++, 'aanderson', 'password', 'Alice', 'Anderson', 'aanderson@revature.com', 'client'),
+    new User(id++, 'bbailey', 'password', 'Bob', 'Bailey', 'bbailey@revature.com', 'client'),
+    new User(id++, 'ccountryman', 'password', 'Charlie', 'Countryman', 'ccountryman@revature.com', 'client'),
+    new User(id++, 'ddavis', 'password', 'Daniel', 'Davis', 'ddavis@revature.com', 'admin'),
+    new User(id++, 'eeinstein', 'password', 'Emily', 'Einstein', 'eeinstein@revature.com', 'client')
 ];
