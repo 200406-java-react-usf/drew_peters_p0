4d76853edab17d6a93ca479712303a55dc93ac91
Drew Peters <peters.drew.dp@gmail.com>
2020-04-29 13:21:19 -0400

    updated repo logic for each data set

--- src/repos/account-repo.ts
+++ src/repos/account-repo.ts
@@ -1,127 +1,82 @@
 import data from '../data/account-db';
 import { Account } from '../models/account';
 import { CrudRepository } from './crud-repo';
-import Validator from '../util/validator';
-import {   
-    BadRequestError, 
+import {
     NotImplementedError, 
-    ResourceNotFoundError, 
+    ResourceNotFoundError
 } from '../errors/errors';
 
 export class AccountRepository implements CrudRepository<Account> {
 
-    private static instance: AccountRepository;
-
-    private constructor() {}
-
-    static getInstance() {
-        return !AccountRepository.instance ? AccountRepository.instance = new AccountRepository() : AccountRepository.instance;
-    }
-
     getAll(): Promise<Account[]> {
 
-        return new Promise<Account[]>((resolve, reject) => {
+        return new Promise<Account[]>((resolve) => {
 
             setTimeout(() => {
-            
-                let accounts = [];
-    
-                for (let account of data) {
-                    accounts.push({...account});
-                }
-        
-                if (accounts.length == 0) {
-                    reject(new ResourceNotFoundError());
-                    return;
-                }
-        
-                resolve;
-        
+                let accounts: Account[] = data;
+                resolve(accounts);
             }, 250);
 
         });
-    };
+    
+    }
 
     getById(id: number): Promise<Account> {
-        return new Promise<Account>((resolve, reject) => {
-            
-            if (!Validator.isValidId(id)) {
-                reject(new BadRequestError());
-            }
+
+        return new Promise<Account>((resolve) => {
 
             setTimeout(() => {
-                
                 const account = {...data.find(account => account.id === id)};
-
-                if(Object.keys(account).length === 0) {
-                    reject(new ResourceNotFoundError());
-                    return;
-                }
-
-                resolve;
-
+                resolve(account);
             }, 250);
 
         });
-    };
+    }
 
-    getAccountByType(type: string): Promise<Account> {
+    getAccountByUniqueKey(key: string, val: string): Promise<Account> {
 
         return new Promise<Account>((resolve, reject) => {
-
-            if (!Validator.isValidStrings(type)) {
-                reject(new BadRequestError());
-                return;
-            }
            
             setTimeout(() => {
-        
-                const account = {...data.filter(account => account.type === type)[0]};
-                
-                if (Object.keys(account).length == 0) {
-                    reject(new ResourceNotFoundError());
-                    return;
-                }
-        
-                resolve;
-        
+                const account = {...data.find(account => account[key] === val)};
+                resolve(account);
             }, 250);
 
         });
         
-    };
     
+    }
+
+    getAccountByType(type: string, oid: number) {
+        
+        return new Promise<Account>((resolve, reject) => {
+        
+            setTimeout(() => {
+                const account = {...data.find(account => account.type === type && account.ownerId === oid)};
+                resolve(account);  
+            }, 250);
+
+        });
+    
+    }
+
     save(newAccount: Account): Promise<Account> {
             
         return new Promise<Account>((resolve, reject) => {
         
-            if (!Validator.isValidObject(newAccount, 'id')) {
-                reject(new BadRequestError('Invalid property values found in provided account.'));
-                return;
-            }
-        
-            setTimeout(() => {
+            setTimeout(() => { 
                 newAccount.id = (data.length) + 1;
                 data.push(newAccount);
-                
-                resolve;
-        
+                resolve(newAccount);
             });
 
         });
     
-    };
+    }
 
     update(updatedAccount: Account): Promise<boolean> {
         
         return new Promise<boolean>((resolve, reject) => {
-
-            let validObj = Validator.isValidObject(updatedAccount);
-            let validId = Validator.isValidId(updatedAccount.id);
-            if (!validObj || !validId) {
-                reject(new BadRequestError('Invalid account provided (invalid values found).'));
-                return;
-            }
         
             setTimeout(() => {
         
@@ -131,27 +86,21 @@ export class AccountRepository implements CrudRepository<Account> {
                     reject(new ResourceNotFoundError('No account found with provided id.'));
                     return;
                 }
-    
+
                 persistedAccount = updatedAccount;
-    
                 resolve(true);
         
             });
 
         });
     
-    };
+    }
 
     deleteById(id: number): Promise<boolean> {
 
         return new Promise<boolean>((resolve, reject) => {
-            
-            if (!Validator.isValidId(id)) {
-                reject(new BadRequestError());
-            }
-
             reject(new NotImplementedError());
-
-            resolve;
         });
-    }};
+    }
+
+}
--- src/repos/user-repo.ts
+++ src/repos/user-repo.ts
@@ -1,10 +1,7 @@
 import data from '../data/user-db';
 import { User } from '../models/user';
 import { CrudRepository } from './crud-repo';
-import Validator from '../util/validator';
-import {  
-    AuthenticationError, 
-    BadRequestError, 
+import {
     NotImplementedError, 
     ResourceNotFoundError, 
     ResourcePersistenceError
@@ -12,33 +9,13 @@ import {
 
 export class UserRepository implements CrudRepository<User> {
 
-    private static instance: UserRepository;
-
-    private constructor() {}
-
-    static getInstance() {
-        return !UserRepository.instance ? UserRepository.instance = new UserRepository() : UserRepository.instance;
-    }
-
     getAll(): Promise<User[]> {
 
-        return new Promise<User[]>((resolve, reject) => {
+        return new Promise<User[]>((resolve) => {
 
             setTimeout(() => {
-            
-                let users = [];
-    
-                for (let user of data) {
-                    users.push({...user});
-                }
-        
-                if (users.length == 0) {
-                    reject(new ResourceNotFoundError());
-                    return;
-                }
-        
-                resolve(users.map(this.removePassword));
-        
+                let users: User[] = data;
+                resolve(users);
             }, 250);
 
         });
@@ -46,48 +23,24 @@ export class UserRepository implements CrudRepository<User> {
     }
 
     getById(id: number): Promise<User> {
-        return new Promise<User>((resolve, reject) => {
-            
-            if (!Validator.isValidId(id)) {
-                reject(new BadRequestError());
-            }
+
+        return new Promise<User>((resolve) => {
 
             setTimeout(() => {
-                
                 const user = {...data.find(user => user.id === id)};
-
-                if(Object.keys(user).length === 0) {
-                    reject(new ResourceNotFoundError());
-                    return;
-                }
-
-                resolve(this.removePassword(user));
-
+                resolve(user);
             }, 250);
 
         });
     }
 
-    getUserByUsername(un: string): Promise<User> {
+    getUserByUniqueKey(key: string, val: string): Promise<User> {
 
         return new Promise<User>((resolve, reject) => {
-
-            if (!Validator.isValidStrings(un)) {
-                reject(new BadRequestError());
-                return;
-            }
            
             setTimeout(() => {
-        
-                const user = {...data.filter(user => user.username === un)[0]};
-                
-                if (Object.keys(user).length == 0) {
-                    reject(new ResourceNotFoundError());
-                    return;
-                }
-        
-                resolve(this.removePassword(user));
-        
+                const user = {...data.find(user => user[key] === val)};
+                resolve(user);
             }, 250);
 
         });
@@ -98,23 +51,10 @@ export class UserRepository implements CrudRepository<User> {
     getUserByCredentials(un: string, pw: string) {
         
         return new Promise<User>((resolve, reject) => {
-
-            if (!Validator.isValidStrings(un, pw)) {
-                reject(new BadRequestError());
-                return;
-            }
         
             setTimeout(() => {
-        
-                const user = {...data.filter(user => user.username === un && user.password === pw).pop()!};
-                
-                if (Object.keys(user).length === 0) {
-                    reject(new AuthenticationError('Bad credentials provided.'));
-                    return;
-                }
-                
-                resolve(this.removePassword(user));
-        
+                const user = {...data.find(user => user.username === un && user.password === pw)};
+                resolve(user);  
             }, 250);
 
         });
@@ -125,26 +65,10 @@ export class UserRepository implements CrudRepository<User> {
             
         return new Promise<User>((resolve, reject) => {
         
-            if (!Validator.isValidObject(newUser, 'id')) {
-                reject(new BadRequestError('Invalid property values found in provided user.'));
-                return;
-            }
-        
-            setTimeout(() => {
-        
-                let conflict = data.filter(user => user.username == newUser.username).pop();
-        
-                if (conflict) {
-                    reject(new ResourcePersistenceError('The provided username is already taken.'));
-                    return;
-                }
-        
-        
+            setTimeout(() => { 
                 newUser.id = (data.length) + 1;
                 data.push(newUser);
-        
-                resolve(this.removePassword(newUser));
-        
+                resolve(newUser);
             });
 
         });
@@ -154,13 +78,6 @@ export class UserRepository implements CrudRepository<User> {
     update(updatedUser: User): Promise<boolean> {
         
         return new Promise<boolean>((resolve, reject) => {
-
-            let validObj = Validator.isValidObject(updatedUser);
-            let validId = Validator.isValidId(updatedUser.id);
-            if (!validObj || !validId) {
-                reject(new BadRequestError('Invalid user provided (invalid values found).'));
-                return;
-            }
         
             setTimeout(() => {
         
@@ -173,6 +90,16 @@ export class UserRepository implements CrudRepository<User> {
                 
                 if (persistedUser.username != updatedUser.username) {
                     reject(new ResourcePersistenceError('Usernames cannot be updated.'));
+                    return;
+                }
+        
+                const conflict = data.find(user => {
+                    if (user.id == updatedUser.id) return false;
+                    return user.email == updatedUser.email; 
+                });
+        
+                if (conflict) {
+                    reject(new ResourcePersistenceError('Provided email is taken by another user.'));
                     return;
                 }
     
@@ -189,19 +116,8 @@ export class UserRepository implements CrudRepository<User> {
     deleteById(id: number): Promise<boolean> {
 
         return new Promise<boolean>((resolve, reject) => {
-            
-            if (!Validator.isValidId(id)) {
-                reject(new BadRequestError());
-            }
-
             reject(new NotImplementedError());
         });
-    }
-
-    private removePassword(user: User): User {
-        let usr = {...user};
-        delete usr.password;
-        return usr;   
     }
 
 }
