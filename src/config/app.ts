8b1d075377b37dc6709e6fb83a34b93c81d3bb91
Drew Peters <peters.drew.dp@gmail.com>
2020-04-29 13:09:38 -0400

    added app.ts to instanciate 1 instance of each service and repo class

---
+++ src/config/app.ts
@@ -0,0 +1,21 @@
+import { UserRepository } from "../repos/user-repo";
+import { UserService } from "../services/user-service";
+import { AccountRepository } from "../repos/account-repo";
+import { AccountService } from "../services/account-service";
+import { TransactionRepository } from "../repos/transaction-repo";
+import { TransactionService } from "../services/transaction-service";
+
+const userRepo = new UserRepository();
+const userService = new UserService(userRepo);
+
+const accountRepo = new AccountRepository();
+const accountService = new AccountService(accountRepo);
+
+const transactionRepo = new TransactionRepository();
+const transactionService = new TransactionService(transactionRepo);
+
+export default {
+    userService,
+    accountService,
+    transactionService
+}
