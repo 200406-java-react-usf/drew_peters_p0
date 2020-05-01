255b1fbb8feabc440e32d94c2870f59f697f4800
Drew Peters <peters.drew.dp@gmail.com>
2020-05-01 18:24:30 -0400

    Updated index to accept db connectivity

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,44 @@
+import dotenv from 'dotenv';
 import express from 'express';
-import bodyparser from 'body-parser';
+import fs from 'fs';
+import morgan from 'morgan';
+import path from 'path';
 
 import { UserRouter } from './router/user-router';
 import { AccountRouter } from './router/account-router';
+import { TransactionRouter } from './router/transaction-router';
+import { AuthRouter } from './router/auth-router';
+import { sessionMiddleware } from './middleware/session-middleware';
+import { corsFilter } from './middleware/cors-filter';
+import { Pool } from 'pg';
 
+// environment configuration
+dotenv.config();
+
+// database configuration
+export const connectionPool: Pool = new Pool({
+    host: process.env['DB_HOST'],
+    port: +process.env['DB_PORT'],
+    database: process.env['DB_NAME'],
+    user: process.env['DB_USERNAME'],
+    password: process.env['DB_PASSWORD'],
+    max: 5
+});
+
+fs.mkdir(`${__dirname}/logs`, () => {});
+const logStream = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' });
+
+// web server configuration
 const app = express();
-
-app.use('/', bodyparser.json());
-
+app.use(morgan('combined', { stream: logStream }));
+app.use(sessionMiddleware);
+app.use(corsFilter);
+app.use('/', express.json());
 app.use('/users', UserRouter);
 app.use('/accounts', AccountRouter);
-// app.use('/transactions', TransactionRouter);
-
+app.use('/transactions', TransactionRouter);
+app.use('/auth', AuthRouter);
 
 app.listen(8080, () => {
-    console.log(`Application runnign and listenting at http://localhost:8080/`)
+    console.log(`Application running and listening at: http://localhost:8080`);
 });
